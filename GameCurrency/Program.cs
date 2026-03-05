742caa82eb34e72694b741df8cf5303d8a4e0d03
kjh3357 <bmj1644035@gmail.com>
2026-03-05 18:01:35 +0900

    37챕터 과제추가제출

--- GameCurrency/Program.cs
+++ GameCurrency/Program.cs
@@ -2,6 +2,11 @@ using System;
 
 GameCurrency wallet1 = new GameCurrency(3, 50);
 GameCurrency wallet2 = new GameCurrency(1, 80);
+GameCurrency sum = wallet1 + wallet2;
+
+Console.WriteLine($"지갑1: {wallet1}");
+Console.WriteLine($"지갑2: {wallet2}");
+Console.WriteLine($"합계: {sum}");
 
 public struct GameCurrency
 {
@@ -14,13 +19,18 @@ public struct GameCurrency
         Silver = silver;
     }
 
-    public override string ToString()
-    {
-        return $"{Gold}G";
-    }
-
     public static GameCurrency operator +(GameCurrency a, GameCurrency b)
     {
-        int sum = a.Gold + b.Gold;
+        int sum = a.Gold + b.Silver;
+        return new GameCurrency(a.Gold, b.Silver);
     }
+    public override string ToString()
+    {
+        return $"{Gold}G {Silver}S";
+    }
+    /*
+    public override int GetTotalSilver()
+    {
+
+    }*/
 }
